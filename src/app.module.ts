19a2dcbb7fdb44b7aba71e750106131fb2c302ac
Sangheon Lee <wwdbsh@gmail.com>
2021-01-27 20:40:46 -0500

    20210127:configservice_update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,41 @@
 import { Module } from '@nestjs/common';
+import * as Joi from "joi";
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from "path";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
+console.log(Joi);
+
 @Module({
-  imports: [GraphQLModule.forRoot({
-    autoSchemaFile:true,
-  }),
-   RestaurantsModule
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+      envFilePath:process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",
+      ignoreEnvFile:process.env.NODE_ENV === "prod",
+      validationSchema:Joi.object({
+        NODE_ENV:Joi.string().valid("dev", "prod").required(),
+        DB_HOST:Joi.string().valid().required(),
+        DB_PORT:Joi.string().valid().required(),
+        DB_USERNAME:Joi.string().valid().required(),
+        DB_PASSWORD:Joi.string().valid().required(),
+        DB_NAME:Joi.string().valid().required(),
+      })
+    }),
+    TypeOrmModule.forRoot({
+      type:"postgres",
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize:true,
+      logging:true
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile:true,
+    }),
+    RestaurantsModule
   ],
   controllers: [],
   providers: [],
