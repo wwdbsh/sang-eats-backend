9102c8ac683484edcc4b1808ad3a2e8c40cb8f42
Sangheon Lee <wwdbsh@gmail.com>
2021-01-31 21:07:48 -0500

    20210131:update

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -5,13 +5,21 @@ import { RestaurantService } from "./restaurants.service";
 
 @Resolver(of => Restaurant)
 export class RestaurantResolver{
-    constructor(private readonly RestaurantService:RestaurantService){}
+    constructor(private readonly restaurantService:RestaurantService){}
     @Query(returns => [Restaurant])
     restaurants():Promise<Restaurant[]>{
-        return this.RestaurantService.getAll();
+        return this.restaurantService.getAll();
     }
     @Mutation(returns => Boolean)
-    createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean{
-        return true;
+    async createRestaurant(
+        @Args("input") createRestaurantDto: CreateRestaurantDto
+    ): Promise<boolean>{
+        try{
+            await this.restaurantService.createRestaurant(createRestaurantDto);
+            return true;
+        }catch(e){
+            console.log(e);
+            return false;
+        }
     }
 }
