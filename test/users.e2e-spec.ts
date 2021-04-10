5fbe0b72f0fe4f4a0971ec2fc57755a0f2453ffc
Sangheon Lee <wwdbsh@gmail.com>
2021-04-10 16:49:22 -0400

    20210410:createAccount testing

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,11 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+jest.mock("got", () => {
+  return {
+    post:jest.fn()
+  };
+});
 const GRAPHQL_ENDPOINT = "/graphql";
 
 describe('UserModule (e2e)', () => {
@@ -45,9 +50,36 @@ describe('UserModule (e2e)', () => {
         expect(res.body.data.createAccount.error).toBe(null);
       });
     });
+
+    it("should fail if account already exists", () => {
+      return request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send({
+        query:`
+          mutation{
+            createAccount(input:{
+              email:"${EMAIL}",
+              password:"12345",
+              role:Owner
+            }){
+              ok
+              error
+            }
+          }
+        `
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data.createAccount.ok).toBe(false);
+        expect(res.body.data.createAccount.error).toBe(
+          "There is a user with that email already"
+        );
+      });
+    });
   });
 
-  it.todo("userProfile");
+  describe("userProfile", () => {
+
+  });
+  
   it.todo("login");
   it.todo("me");
   it.todo("verifyEmail");
