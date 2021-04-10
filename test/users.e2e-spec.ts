03f140eb73aeb02e17a565e66af1d50a10b61bb6
Sangheon Lee <wwdbsh@gmail.com>
2021-04-10 16:38:14 -0400

    20210410:createAccount testing

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,8 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+const GRAPHQL_ENDPOINT = "/graphql";
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
@@ -20,7 +22,31 @@ describe('UserModule (e2e)', () => {
     app.close();
   });
 
-  it.todo("createAccount");
+  describe("createAccount", () => {
+    const EMAIL = "sang@albany.com";
+    it("should create account", () => {
+      return request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send({
+        query:`
+          mutation{
+            createAccount(input:{
+              email:"${EMAIL}",
+              password:"12345",
+              role:Owner
+            }){
+              ok
+              error
+            }
+          }
+        `
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data.createAccount.ok).toBe(true);
+        expect(res.body.data.createAccount.error).toBe(null);
+      });
+    });
+  });
+
   it.todo("userProfile");
   it.todo("login");
   it.todo("me");
