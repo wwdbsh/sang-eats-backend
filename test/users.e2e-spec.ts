2a597819ccd35e7fc811756fc49f30c7fa9261bf
Sangheon Lee <wwdbsh@gmail.com>
2021-04-10 16:25:47 -0400

    20210410:e2e test setup

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { getConnection } from 'typeorm';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -13,10 +14,16 @@ describe('UserModule (e2e)', () => {
     app = module.createNestApplication();
     await app.init();
   });
-  it.todo("me");
-  it.todo("userProfile");
+
+  afterAll(async () => {
+    await getConnection().dropDatabase();
+    app.close();
+  });
+
   it.todo("createAccount");
+  it.todo("userProfile");
   it.todo("login");
-  it.todo("editProfile");
+  it.todo("me");
   it.todo("verifyEmail");
+  it.todo("editProfile");
 });
